4360c51c6afd77c2e1a2c1cf654b271fc2e250a9
BlankTo <80112405+BlankTo@users.noreply.github.com>
2024-08-12 12:16:21 +0200

    Update all.py

--- all.py
+++ all.py
@@ -4,3 +4,4 @@ from utils.args import args
 if __name__ == "__main__":
     main()
 
+## call with: c:/Users/User/Desktop/aml_project/aml23-ego-master/aml_venv/Scripts/python.exe c:/Users/User/Desktop/aml_project/aml23-ego-master/all.py config=configs/I3D_save_feat.yaml dataset.shift=D1-D1 dataset.RGB.data_path=ek_data\frames n_frame=25
