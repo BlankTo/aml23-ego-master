1af666e9014fe9b4ed0b5f9509390a67b12b4758
BlankTo <80112405+BlankTo@users.noreply.github.com>
2024-08-12 21:51:22 +0200

    ...

--- all.py
+++ all.py
@@ -1,7 +1,10 @@
 from save_feat import main
 from utils.args import args
+from cluster_analysis import analyze_clusters
 
 if __name__ == "__main__":
     main()
+
+    analyze_clusters(args.name, [4, 8, 16, 64])
 
 ## call with: c:/Users/User/Desktop/aml_project/aml23-ego-master/aml_venv/Scripts/python.exe c:/Users/User/Desktop/aml_project/aml23-ego-master/all.py config=configs/I3D_save_feat.yaml dataset.shift=D1-D1 dataset.RGB.data_path=ek_data\frames n_frame=25
